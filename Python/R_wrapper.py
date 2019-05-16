c5a5dec925d41d068f2da7d32e86dee9e7aa78e4
Nathan Hughes <nathan1hughes@gmail.com>
2019-05-16 13:40:07 +0100

    R Wrapper is up to standard

--- Python/R_wrapper.py
+++ Python/R_wrapper.py
@@ -4,29 +4,39 @@ from os import chdir
 from rpy2.robjects import pandas2ri
 import rpy2.robjects as robjects
 import rpy2.robjects as ro
+from rpy2.robjects.packages import importr
 import pandas as pd
-
 
 chdir("/Users/hughesn/Transcripts/RNA-Seq/Analysis/R/")
 r = robjects.r
-r.source("../R/analysis.R")
 
+r.source("./prep_data.R")
+edgeR = importr('edgeR')
+stringr = importr('stringr')
+gProfileR = importr('gProfileR')
+DESeq2 = importr('DESeq2')
+pheatmap = importr('pheatmap')
+apeglm = importr('apeglm')
+base = importr('base')
+dollar = base.__dict__["$"]
 
-treats = list(set(list(r('as.vector(dds$treatmentID)'))))
+data_loc = "../../arabidopsis_thaliana/seedling_data/htseq-count_universal_removal"
+dds_setup = r['get_DESeq2'](data_loc)
+dds = r['DESeq'](dds_setup)
+res = r['results'](dds)
+
+treats = list(set(list(r['as.vector'](dollar(dds, 'treatmentID')))))
 
 writer = pd.ExcelWriter(
     '../Data/significantly_different_expressions_no_adaptors.xlsx')
 
 for i, j in combos(treats, 2):
-
-    r_res = r.lfcShrink(r["dds"], contrast=robjects.StrVector(
+    r_res = r.lfcShrink(dds, contrast=robjects.StrVector(
         ["treatmentID", i, j]))
     r_res = r['as.data.frame'](r_res)
-
     with localconverter(ro.default_converter + pandas2ri.converter):
         df = ro.conversion.rpy2py(r_res)
         df = df.sort_values(by=['padj'])
-        # df = df[df['padj'] < 0.01]
         conv = {k: str(k) for k in list(df.columns)}
         df = df.rename(index=str, columns=conv)
         df.to_excel(writer, sheet_name="{0} | {1}".format(i, j))
--- R/analysis.R
+++ R/analysis.R
@@ -13,16 +13,3 @@ dds.setup <- get_DESeq2(data.loc)
 
 dds <- DESeq(dds.setup)
 res <- results(dds)
-
-
-
-
-## Check if the controls have any differences
-#results(dds, contrast = c("repID", "a1", "b2"))
-
-## res_control_c_v_w <- results(dds, contrast = c("treatmentID", "col_w_05h", "col_c_05h"))
-## res_control_c_v_w <- res_control_c_v_w[order(res_control_c_v_w$pvalue),]
-
-
-## res_col_0_T<- results(dds, contrast = c("treatmentID", "col_w_05h", "col_c_6h"))
-## res_col_0_T <- res_col_0_T[order(res_col_0_T$pvalue),]
